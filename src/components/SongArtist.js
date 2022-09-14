01a076fa67a43eb65215767563d3750b302c2679
glorialunar <glorialunar2509@gmail.com>
2022-09-14 19:46:50 -0300

    add styles to the artist and lyric section

--- src/components/SongArtist.js
+++ src/components/SongArtist.js
@@ -1,15 +1,28 @@
+import styles from 'styled-components';
+
 function SongArtist({artist}) {
     // console.log(artist);
     return (
-        <section>
-            <h3>{artist.strArtist}</h3>
-            <img src={artist.strArtistThumb} alt={artist.strArtist}/>
-            <p>{artist.intBornYear}</p>
-            <p>{artist.strCountry}</p>
-            <p>{artist.strGenre}</p>
-            <a href={artist.strLastFMChart}>Sitio Web</a>
-        </section>
+        <>
+            <SongArtistContainer>
+                <h3>{artist.strArtist}</h3>
+                <img src={artist.strArtistThumb} alt={artist.strArtist}/>
+                <p><b>Born Year: </b>{artist.intBornYear}</p>
+                <p><b>Country: </b>{artist.strCountry}</p>
+                <p><b>Genre: </b>{artist.strGenre}</p>
+                <a href={artist.strLastFMChart}>Sitio Web</a>
+            </SongArtistContainer>
+        </>
     )
 }
 
 export default SongArtist;
+
+const SongArtistContainer = styles.section`
+    margin: 2rem 0;
+
+    img{
+        width: 90vw;
+        border-radius: 10px;
+    }
+`
--- src/components/SongLyric.js
+++ src/components/SongLyric.js
@@ -1,7 +1,7 @@
 function SongLyric({title, lyric}) {
     return (
         <section>
-            <h3>{title}</h3>
+            <h3>Song: {title}</h3>
             <blockquote style={{whiteSpace: "pre-wrap"}}>{lyric}</blockquote>
         </section>
     )
